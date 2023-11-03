234e11e9d0ba50320a1abd12ed56f09eb25d3011
BriceBZH <brice.rubeaux@wanadoo.fr>
2023-11-03 14:20:46 +0000

    suite bonus

--- projet-paint/assets/js/main.js
+++ projet-paint/assets/js/main.js
@@ -17,31 +17,48 @@ function getSelectedColor()
 
     return null;
 }
-
+function create_color() {
+  let randomColor = (Math.floor(Math.random()*0xFFFFFF)).toString(16);
+  return "#"+randomColor;
+}
 function loadPalette(palette)
 {
     let colors = palette;
-    let div = document.querySelectorAll("header > div");
+    //bonus2
+    let head = document.querySelector("header");
     let nb_colors = colors.length;
-    for(let j=0;j < nb_colors;j++ ) {
-        div[j].setAttribute("style", "background-color:"+colors[j]+";")
+    //fenetre pour savoir combien de couleur on veut
+    let nb_palette_color = prompt("Combien de couleur voulez-vous, minimum 8", "8");
+    if(nb_palette_color != null && nb_palette_color == 0)  {
+        nb_palette_color = nb_colors;
     }
+    let manque_color = nb_palette_color - nb_colors;
+    for(let i=0;i<manque_color;i++) {
+        let newDiv = document.createElement("div");
+        head.appendChild(newDiv);
+        let new_color = colors.push(create_color());
+        console.log(new_color);
+    }
+    //affichage des couleurs dans les div
+    let div = document.querySelectorAll("header > div");
+    let nb_colors_bis = colors.length;
+    for(let j=0;j < nb_colors_bis;j++ ) {
+        div[j].setAttribute("style", "background-color:"+colors[j]+";");
+        let newInput = document.createElement("input");
+        newInput.setAttribute("type", "color");
+        newInput.setAttribute("value", colors[j]);
+        div[j].appendChild(newInput);
+    }
+    //on modifie le div en fonction de la couleur mise dans le input
+    let input = document.querySelectorAll("header > div > input");
+    console.log(input);
 }
 
 
 window.addEventListener("DOMContentLoaded", function(){
     loadPalette(["#22f6f3", "#3daf7e", "#ffffff", "#ec8236", "#a9a7ee", "#ecc606", "#f783f2", "#e89e80"]);
-    let div = document.querySelectorAll("header > div");
-    //bonus2
-    let head = document.querySelector("header");
-    //fenetre pour savoir combien de couleur on veut
-    let nb_palette_color = 16;
-    let nb_color = loadPalette.length();
-    let manque_color = nb_palette_color - nb_color;
-    for(let i=0;i<manque_color;i++) {
-        
-    }
     //exo2
+    let div = document.querySelectorAll("header > div");
     let nb_div = div.length;
     for(let i=0;i<nb_div;i++) {
         div[i].addEventListener("click", selectColor);
