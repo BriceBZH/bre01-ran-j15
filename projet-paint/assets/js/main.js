c67a6aa31eee04085111da33307c01810df44f47
BriceBZH <brice.rubeaux@wanadoo.fr>
2023-11-03 11:34:43 +0000

    fin exos + début bonus

--- projet-paint/assets/js/main.js
+++ projet-paint/assets/js/main.js
@@ -5,6 +5,7 @@ function selectColor(event)
     let color = colorBox.style.backgroundColor;
 
     sessionStorage.setItem("selectedColor", color);
+    console.log(getSelectedColor());
 }
 
 function getSelectedColor()
@@ -31,14 +32,31 @@ function loadPalette(palette)
 window.addEventListener("DOMContentLoaded", function(){
     loadPalette(["#22f6f3", "#3daf7e", "#ffffff", "#ec8236", "#a9a7ee", "#ecc606", "#f783f2", "#e89e80"]);
     let div = document.querySelectorAll("header > div");
+    //bonus2
+    let head = document.querySelector("header");
+    //fenetre pour savoir combien de couleur on veut
+    let nb_palette_color = 16;
+    let nb_color = loadPalette.length();
+    let manque_color = nb_palette_color - nb_color;
+    for(let i=0;i<manque_color;i++) {
+        
+    }
+    //exo2
     let nb_div = div.length;
     for(let i=0;i<nb_div;i++) {
-        div[i].addEventListener("click", function(selectColor){
-        	console.log(getSelectedColor());
+        div[i].addEventListener("click", selectColor);
+    }
+    //exo3
+    let div_main = document.querySelectorAll("main > div > div");
+    let nb_div_main = div_main.length;
+    for(let i=0;i<nb_div_main;i++) {
+        div_main[i].addEventListener("click", function(){
+            if(!div_main[i].style.backgroundColor) {
+                div_main[i].setAttribute("style", " background:"+getSelectedColor()+";");
+            } else {
+                div_main[i].setAttribute("style", null);
+            }
         });
     }
-
-
-    // le code de l'étape 3 se passe ici
 
 });
